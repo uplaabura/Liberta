155413fa03b8cfca1852fc9e0c99e39271423b20
uplaabura <uplaabura@gmail.com>
2015-09-07 16:00:17 +0800

    crew pax control v3.1
    Archive Devise views, leave only sessions view
    Make Devise views controllable by config.scoped_views = true
    Deduce Devise functions (***able)
    Remove http_basic_authenticate_with in articles_controller
    Modify initiallizers/devise.rb to retrieve email from facebook by add
    info_fields
    Add name & image column by db migration
    Replenish comments to crew.rb & facebook_callback_controller.rb
    
    Summary:
    Now Liberta can retrieve info from Facebook and save into database

--- app/models/crew.rb
+++ app/models/crew.rb
@@ -1,36 +1,83 @@
 class Crew < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+
+  # Include default devise modules. 
   devise  :database_authenticatable,
-          :recoverable,  
-          :registerable,
-          :rememberable, 
+          #:confirmable,
+          #:recoverable,  
+          #:registerable,
+          #:rememberable, 
           :trackable, 
-          :validatable,
+          #:timeoutable,
+          #:validatable,
+          #:lockable,
 
           :omniauthable, :omniauth_providers => [:facebook]
 
+          
   #def self.koala(auth)
     #access_token = auth['token']
     #facebook = Koala::Facebook::API.new(access_token)
     #facebook.get_object("me?fields=email,name,picture")
   #end
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |crew| #first_or_create is a rails method
-      crew.email = auth.info.email
-      crew.password = Devise.friendly_token[0,20]
-      #crew.name = auth.info.name   # assuming the user model has a name
-      #crew.image = auth.info.image # assuming the user model has an image
+  def self.find_record_from_omniauth_facebook(auth) #find record then update or create from omniauth_facebook
+    if the_crew = Crew.where({:provider => auth.provider, :uid => auth.uid,}).first
+      Crew.update( #.update is an ActiveRecord::Relation method, requires .update(id, attributes)
+        the_crew[:id],
+        { :email => auth.info.email, 
+          :name => auth.info.name, 
+          :image => auth.info.image
+        }
+      )
+      return the_crew
+    else # Create a user with a stub password. 
+      Crew.create(
+        { :provider => auth.provider,
+          :uid => auth.uid,
+          :email => auth.info.email, 
+          :name => auth.info.name, 
+          :image => auth.info.image,
+          :password => Devise.friendly_token[0,20]
+        }
+      )
     end
   end
 
-  #def self.new_with_session(params, session)
-    #super.tap do |crew|
-      #if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        #crew.email = data["email"] if crew.email.blank?
-      #end
+  #def self.find_record_from_omniauth_facebook(auth) #find record or create from omniauth_facebook
+    #where({:provider => auth.provider, :uid => auth.uid}).first_or_create do |create_crew| #first_or_create is a ActiveRecord::relative method
+      #create_crew.email = auth.info.email
+      #create_crew.password = Devise.friendly_token[0,20]
+      #create_crew.name = auth.info.name   
+      #create_crew.image = auth.info.image 
     #end
   #end
+
+  #def self.new_with_session(params, session)
+    
+    #super.tap do |crew|
+      #if crew_info = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
+        #crew.email = crew_info["email"] if crew.email.blank?
+      #end
+    #end
+
+    #super.tap do |crew_info|
+      #if hash_from_facebook = session["devise.facebook_data"]
+        #crew_info.email = hash_from_facebook.info.email
+        #crew_info.name = hash_from_facebook.info.name
+        #crew_info.image = hash_from_facebook.info.image
+      #end
+    #end
+  
+  #end
+
+  #But when is this method used? 
+  #It's used by Devise when something goes wrong when creating your user. 
+  #Imagine that the authentication provider doesn't give you an email
+  #Well, you can redirect the user to your sign up page where he can complete the signup process. 
+  #But if you redirect the user, you lose the data received by the oauth. 
+  #The solution is to put this data into the session.
+  #So in controller
+  ##session["devise.facebook_data"] = env["omniauth.auth"]
+
 
 end
--- db/migrate/20150905081036_add_columns_to_crews.rb
+++ db/migrate/20150905081036_add_columns_to_crews.rb
@@ -2,5 +2,6 @@ class AddColumnsToCrews < ActiveRecord::Migration
   def change
     add_column :crews, :provider, :string
     add_column :crews, :uid, :string
+ 
   end
 end
